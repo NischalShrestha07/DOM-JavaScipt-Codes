4017032d0e716d585846b572ccb9ce6f50b2670a
Nischal <nischalshrestha800@gmail.com>
2024-01-14 13:03:54 +0545

    .

---
+++ hello/age.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+
+
+age = prompt("Enter your age:");
+
+
+if (age > 10 && age < 20) {
+    console.log("YOur age lies between 10 and 20");
+
+
+}
+else {
+    console.log("Your age doesn't lies between 10 and 20");
+}
