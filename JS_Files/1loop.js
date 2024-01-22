da86170a5314d88ab7853b6c0e2b4aab20833c7a
Nischal <nischalshrestha800@gmail.com>
2024-01-22 18:06:18 +0545

    .

---
+++ JS_Files/1loop.js
@@ -0,0 +1,59 @@
+//********WHILE LOOPS********
+
+// const list = ['a', 'b', 'c', 'd']
+// let i = 0
+// while (i < list.length) {
+//     console.log(list[i]);
+//     console.log(i);
+//     i = i + 1
+
+// }
+
+
+//********* */ use of concat() to join the two list********
+
+// const a = [1, 2]
+// const b = [3, 4, 5, 14, 12, 45]
+// const c = a.concat(b)
+// console.log(c);
+
+//About multi dimensional matrix
+// const matrix = [
+//     [1, 2, 3],// Here the index starts from the 0 from left to right & top to bottom.
+//     [4, 5, 6],
+//     [7, 8, 9]
+
+// ]
+
+// console.log(
+
+//     matrix[0][2]//the [0] will take the vertical index which is first row as the index is 0 and [2] will take horizontal index which is 3.
+
+
+// );
+// console.log(
+
+//     matrix[2][2]
+// );
+
+
+
+//********** DO WHILE LOOPS*********
+// const list = ['A', 'B', 'C', 'D']
+// let i = 0
+// do {
+//     console.log(list[i]);
+//     console.log(i);
+//     i = i + 1
+// } while (i < list.length)
+
+
+
+// *******FOR LOOPS*********
+// const list = ['nischal', 'naruto', 'itachi', 'sasuke']
+// for (i = 0; i < list.length; i++) {
+//     console.log(list[i]);
+//     console.log(i);
+
+// }
+
