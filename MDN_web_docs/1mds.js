6638c52e53b1d93abbc423f2045d399ebac6dc65
Nischal <nischalshrestha800@gmail.com>
2024-01-23 23:05:13 +0545

    .

---
+++ MDN_web_docs/1mds.js
@@ -0,0 +1,41 @@
+// let minute = 30
+// function minTosec(minute) {
+//     return minute * 60
+
+// }
+// console.log(
+//     "The entered minute is equal to the " + minTosec(30) + " seconds"
+// );
+
+
+// *** toget the increment value by 1***
+// function number(num) {
+//     return num + 1
+
+// }
+// x = number(12)
+// console.log(x);
+
+
+// // ***year to days Convertor
+// function yearToDays(year) {
+//     return year * 365
+
+// }
+
+// x = yearToDays(100)
+// console.log(x);
+
+
+function number(n1, n2) {
+    sum = n1 + n2
+    if (sum <= 100) {
+        console.log("True");
+
+    } else {
+        console.log("False");
+    }
+}
+
+x = number(15, 56)
+console.log(x);
