7625b301b1fd0803a824088e2f80ff0bf79d370f
Nischal <nischalshrestha800@gmail.com>
2024-02-11 20:51:58 +0545

    .

--- JS_Files/p1.js
+++ JS_Files/p1.js
@@ -1,15 +1,30 @@
 // const prompt = require("prompt-sync")();
 
 // let age = prompt("Enter Your Age")
-age = Number.parseInt(age)
+// age = Number.parseInt(age)
 
-const canDrive = (age) => {
-    age >= 18 ? true : false
-}
+// const canDrive = (age) => {
+//     age >= 18 ? true : false
+// }
 
-if (canDrive(age)) {
-    alert("Yes You can Drive")
-}
-else {
-    alert("You cannot Drive")
+// if (canDrive(age)) {
+//     alert("Yes You can Drive")
+// }
+// else {
+//     alert("You cannot Drive")
+// }
+let runAgain = true
+while (runAgain) {
+    let age = prompt("Enter your age")
+    age = Number.parseInt(age)
+    const canDrive = (age) => {
+        return age >= 18 ? true : false
+    }
+    if (canDrive(age)) {
+        alert("You can Drive")
+    }
+    else {
+        alert("You cannot Drive")
+    }
+    runAgain == confirm("Do you want to play again?")
 }
