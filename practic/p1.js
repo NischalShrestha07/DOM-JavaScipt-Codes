083765aa373645923095052f1ea7438821f25284
Nischal <nischalshrestha800@gmail.com>
2024-02-13 09:30:03 +0545

    .

---
+++ practic/p1.js
@@ -0,0 +1,7 @@
+
+const changeBgRed = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+let b = document.body;
+console.log("First Child of b is", b.firstChild);
+console.log("First Element child of the b is:", b.firstElementChild);
