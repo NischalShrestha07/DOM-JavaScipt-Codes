1f6e60d911abdd6188c1bb58b36267372f97182e
Nischal <nischalshrestha800@gmail.com>
2024-03-06 22:17:15 +0545

    .

---
+++ Great/loop.js
@@ -0,0 +1,15 @@
+//for loops
+
+// for (let i = 0; i < =10; i++) {
+//     console.log("Nischal Shrestha");
+//     // console.log(i);
+//     // console.log();
+    
+// }
+
+
+let coding=["Javascript","Python","C++"]
+for (let i = 0; i < coding.length; i++) {
+console.log(coding[i])
+    
+}
