b46c94ea79e3be774e84eb631042fd1d6d995ec7
Nischal <nischalshrestha800@gmail.com>
2024-03-06 22:19:38 +0545

    .

--- Great/loop.js
+++ Great/loop.js
@@ -10,6 +10,16 @@
 
 let coding=["Javascript","Python","C++"]
 for (let i = 0; i < coding.length; i++) {
-console.log(coding[i])
+    
+    console.log(coding[i])
+}
+
+
+for (let i = 0; i <=5; i++) {
+    // const element = array[i];
+    console.log(i);
+    for (let j = 0; j <=3 ; j++) {
+console.log("Inner Loops"+j);        
+    }
     
 }
