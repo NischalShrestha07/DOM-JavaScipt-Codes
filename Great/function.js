883f1b1313f84d988b9aa4ea548a89ab8b03f94a
Nischal <nischalshrestha800@gmail.com>
2024-03-06 23:30:17 +0545

    .

---
+++ Great/function.js
@@ -0,0 +1,11 @@
+function Person(){
+    this.firstName="Nischal",
+    this.lastName="Shrestha"
+
+}
+
+const person1=new Person()
+const person2=new Person()
+
+console.log(person1)
+console.log(person2);
