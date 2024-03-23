3fa7c6720b5fcea8911c27f35516db028da7a5a6
Nischal <nischalshrestha800@gmail.com>
2024-03-23 09:02:36 +0545

    .

---
+++ Dom_Demo/loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+
+{
+    for (int i = 0; i < 100; i++)
+    {
+        cout << "Nischal\n";
+    }
+}
---
+++ Dom_Demo/try.py
@@ -0,0 +1,4 @@
+a=10
+b=20
+sum=a+b
+print("The sum of the entered number is:",sum)
