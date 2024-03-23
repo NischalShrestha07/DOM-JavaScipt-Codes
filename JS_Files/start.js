78576ba71cda7d5993350f55eaf4c4e5eecdae66
Nischal <nischalshrestha800@gmail.com>
2024-03-23 09:21:06 +0545

    .

--- JS_Files/start.js
+++ JS_Files/start.js
@@ -1,1 +1,9 @@
-console.log(years["2"] != years["02"]);
+// console.log(years["2"] != years["02"]);
+const fruits=[];
+fruits.push("banana","apple","peach")
+// fruits.pop(2,"peach")
+fruits.pop("peach")
+fruits.insert("Licchi","Orange")
+console.log(fruits);
+
+console.log(fruits.length);
