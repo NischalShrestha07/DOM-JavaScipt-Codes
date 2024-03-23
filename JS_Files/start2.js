25bf494f35f8f98a3f0902f5f54cd704f7e18d11
Nischal <nischalshrestha800@gmail.com>
2024-03-23 09:51:41 +0545

    .

--- JS_Files/start2.js
+++ JS_Files/start2.js
@@ -1,4 +1,26 @@
-fruits[5]="mango"
-console.log(fruits[5]);
-console.log(Object.keys(fruits));
-console.log(fruits.length);
+// fruits[5]="mango"
+// console.log(fruits[5]);
+// console.log(Object.keys(fruits));
+// console.log(fruits.length);
+
+
+// const array1=['a','b','c']
+const array1=['mango','orange','kiwi']
+const iterator1=array1[Symbol.iterator]();
+
+for(const value of iterator1)
+{
+    console.log(value);
+
+}
+
+
+
+const arr=['hari','shuam','ram','krishna']
+const  letterResult=document.getElementById("letterResult")
+for(const letter of arr)
+{
+    const li=document.createElement("li")
+    li.textContent=Letter
+    letterResult.appendChild(FileList)
+}
