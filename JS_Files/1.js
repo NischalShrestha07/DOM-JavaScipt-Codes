bd70a0e4e31a169827b0655fa7822a6860b09f70
Nischal <nischalshrestha800@gmail.com>
2024-03-26 22:54:04 +0545

    .

--- JS_Files/1.js
+++ JS_Files/1.js
@@ -1,1 +1,24 @@
-window.location.href = "https://www.google.com";
+// window.location.href = "https://www.google.com";
+ let dayNumber = 6;
+if (dayNumber == 1) {
+            console.log("Today is Sunday.");
+        }
+ elif(dayNumber == 2) {
+            console.log("Today is Monday.")
+        }
+        elif(dayNumber == 3) {
+            console.log("Today is Tuesday.")
+        }
+        elif(dayNumber == 4) {
+            console.log("Today is Wednesday.")
+        }
+        elif(dayNumber == 5) {
+            console.log("Today is Thrusday.")
+        }
+
+        elif(dayNumber == 6) {
+            console.log("Today is Friday.")
+        }
+        // else {
+        //     console.log("Today is Saturday.")
+        // }
---
+++ JS_Files/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+ // else {
+        //     console.log("Today is Saturday.")
+        // }
