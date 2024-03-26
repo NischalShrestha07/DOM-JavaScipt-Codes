95814d0a6aee806ac2def701c817b08b9ee283d3
Nischal <nischalshrestha800@gmail.com>
2024-03-27 01:45:49 +0545

    ,

---
+++ Bro/script.js
@@ -0,0 +1,14 @@
+
+
+let firstName="Nischal"
+let age=21
+let student=true 
+
+console.log("Hello",firstName)
+console.log("You are",age,"years old")
+console.log("Enrolled",student);
+
+
+document.getElementById("p1").innerHTML="Hello "+firstName
+document.getElementById("p2").innerHTML="You are "+age+" years old"
+document.getElementById("p3").innerHTML="Enrolled "+student
