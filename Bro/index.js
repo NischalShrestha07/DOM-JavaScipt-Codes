4d448d3a50436276eeca12315da07fc9bd606ca9
Nischal <nischalshrestha800@gmail.com>
2024-03-27 02:23:30 +0545

    .

--- Bro/index.js
+++ Bro/index.js
@@ -1,10 +1,21 @@
-// let student=20
-// student=student+1
-// console.log(student);
+// // let student=20
+// // student=student+1
+// // console.log(student);
 
-// usename=window.prompt("Whats your name:")
-// console.log(username);
+// // usename=window.prompt("Whats your name:")
+// // console.log(username);
 
-// a=120
-// b=120
-// console.log(a+b);
+// // a=120
+// // b=120
+// // console.log(a+b);
+// let age=window.prompt("How old are you")
+// age+=1
+// console.log("Happy Birthday! You are",age,"years old");
+
+let name = prompt('What is your name?');
+
+if (name) {
+  alert(`Hello, ${name}!`);
+} else {
+  alert('Hello, stranger!');
+}
