417bd3b9e9252add2deedf9dfb823bf427b14dad
Nischal <nischalshrestha800@gmail.com>
2024-03-27 02:18:23 +0545

    .

---
+++ Bro/index.js
@@ -0,0 +1,10 @@
+// let student=20
+// student=student+1
+// console.log(student);
+
+// usename=window.prompt("Whats your name:")
+// console.log(username);
+
+// a=120
+// b=120
+// console.log(a+b);
--- Bro/script.js
+++ Bro/script.js
@@ -1,14 +1,24 @@
 
 
-let firstName="Nischal"
-let age=21
-let student=true 
+// let firstName="Nischal"
+// let age=21
+// let student=true 
 
-console.log("Hello",firstName)
-console.log("You are",age,"years old")
-console.log("Enrolled",student);
+// console.log("Hello",firstName)
+// console.log("You are",age,"years old")
+// console.log("Enrolled",student);
 
 
-document.getElementById("p1").innerHTML="Hello "+firstName
-document.getElementById("p2").innerHTML="You are "+age+" years old"
-document.getElementById("p3").innerHTML="Enrolled "+student
+// document.getElementById("p1").innerHTML="Hello "+firstName
+// document.getElementById("p2").innerHTML="You are "+age+" years old"
+// document.getElementById("p3").innerHTML="Enrolled "+student
+
+// let username=window.prompt("Whats your name:")
+// console.log(username);
+let username
+document.getElementById("myButton").onclick=function(){
+    username=document.getElementById("myText").valu0;
+    // username=document.getElementById("myText");
+    console.log(username);
+    document.getElementById("myLabel").innerHTML="HEllo "+username
+}
