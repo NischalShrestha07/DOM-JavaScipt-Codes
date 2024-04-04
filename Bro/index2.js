28f48b78947c64ad04f747701932b30ce25fddff
Nischal <nischalshrestha800@gmail.com>
2024-04-04 08:43:28 +0545

    .

--- Bro/index2.js
+++ Bro/index2.js
@@ -7,4 +7,26 @@
 //     return releaseEvents
 // }
 
-let prices=[]
+let userName="Bro"
+let userInbox=0
+
+login()
+// displayUserName()
+// displayUserInbox()
+
+function login(){
+    displayUserName()
+displayUserInbox()
+
+function displayUserName()
+{
+    console.log("Welcome",userName);
+
+}
+
+function displayUserInbox()
+{
+    console.log(`You have ${userInbox} new messages`);
+}
+}
+
