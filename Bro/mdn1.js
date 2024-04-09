94dce3195fa696ed5a9f7bce63d9d61f268936a8
Nischal <nischalshrestha800@gmail.com>
2024-04-09 23:19:01 +0545

    .

---
+++ Bro/mdn1.js
@@ -0,0 +1,22 @@
+class Rectangle{
+    constructor(height,width)
+    {
+        this.height=height
+        this.width=width
+
+    }
+}
+const Rectangle=class{
+    constructor(height,width)
+    {
+        this,height=height
+        this.width=width
+    }
+};
+const Rectangle=class Rectangle2{
+    constructor(height,width)
+    {
+        this.height=height
+        this.width=width
+    }
+}
