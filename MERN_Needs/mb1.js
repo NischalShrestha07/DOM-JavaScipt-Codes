7e9aff24696437bcb871602a8d7f428406d149de
Nischal <nischalshrestha800@gmail.com>
2024-04-30 19:39:28 +0545

    JS Files

---
+++ MERN_Needs/mb1.js
@@ -0,0 +1,21 @@
+
+const name1="Manish"
+const ageOfName1=22
+const professionOfName1="Student"
+
+console.log(name1);
+console.log(ageOfName1);
+console.log(professionOfName1);
+
+
+console.log("");
+const Person ={
+    "name":"Nischal",
+    "age":21,
+    "profession":"Developer"
+
+}
+
+console.log(Person.name);
+console.log(Person.age);
+console.log(Person.profession);
