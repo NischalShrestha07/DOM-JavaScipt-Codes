ba25a40013e9698ddffabd15cde4a3a70de43b63
Nischal <nischalshrestha800@gmail.com>
2024-05-01 23:49:50 +0545

    JavaScript  Details

--- MERN_Needs/earn1.js
+++ MERN_Needs/earn1.js
@@ -5,4 +5,13 @@
 // say('Nischal Shrestha')
 function add (){
     let sum =0
+    for(let i=0;i<arguments.length;i++)
+    {
+        sum+=arguments[i]
+
+    }
+    return sum
+
 }
+result=add(45,5,45,10)
+console.log(result);
